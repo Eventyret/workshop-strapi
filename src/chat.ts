baf612ed60322e4576921617b465e0850a708348
Marc <marc.roig.campos@strapi.io>
2024-05-16 18:21:56 +0200

    chore: fine tune prompt

--- src/chat.ts
+++ src/chat.ts
@@ -12,28 +12,42 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const systemPrompt = `
-You are a helpful assistant helping a content editor create their content structure, the user will prompt you about content creation and your job is to help them create a content type schema.
+You will create a Strapi Content Type Schema.
+
+
+You will also be challenging the schema and ask for more information about
+the domain to improve it.
+
+Do not summarize the fields you are creating on the schema on the text action
+if not asked by the user.
+
+The text action is mandatory.
 
 Example:
-
 I want to create a restaurant, which fields should I be creating?
 
-Valid response:
+Valid Response:
   {
-    "type": "content-type",
-    "content": {
-      "attributes": {
-        "name": {
-          "type": "text"
-        },
-        "position": {
-          "type": "geopoint"
-        }
+    "actions": [
+      {
+        "type": "text",
+        "content": "Here are some proposals"
+      },
+      {
+        "type": "content-type",
+        "content": {
+          "attributes": {
+            "name": {
+              "type": "text"
+            },
+            "position": {
+              "type": "coordinates"
+            }
+          }
       }
-    }
-  }
+    ]
 
-(Use a json format)
+  }
 `;
 
 const main = async () => {
@@ -58,13 +72,12 @@ const main = async () => {
   const translator = createJsonTranslator(model, validator);
 
   // Process requests interactively or from the input file specified on the command line
-
   const response = await translator.translate(
-    "I want to create a restaurant, which fields should I be creating?",
+    "I want to create a country content type, could you help me create it?",
     systemPrompt
   );
 
-  console.log(response);
+  console.log(JSON.stringify(response.data, null, 2));
 };
 
 main();
--- src/response-schema.ts
+++ src/response-schema.ts
@@ -1,8 +1,17 @@
+/**
+ * Mandatory response action.
+ * Should briefly explain the proposed changes, ask for more information
+ * and challenge the proposed schema (if any).
+ */
 type TextResponse = {
   type: "text";
   content: string;
 };
 
+/**
+ * Content type response action.
+ * Proposed as soon there is minimal information to create a content type.
+ */
 type ContentTypeResponse = {
   type: "content-type";
   content: {
@@ -14,5 +23,8 @@ type ContentTypeResponse = {
   };
 };
 
-// export type LLMResponse = (TextResponse | ContentTypeResponse)[];
-export type LLMResponse = ContentTypeResponse;
+type Response = TextResponse | ContentTypeResponse;
+
+export type LLMResponse = {
+  actions: Response[];
+};
